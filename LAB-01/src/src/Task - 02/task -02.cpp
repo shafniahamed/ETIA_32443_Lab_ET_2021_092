70145c3c49bcf6eab0de4a411d46b38b7332c02f
shafniahamed <shafnim-et21092@stu.kln.ac.lk>
2026-05-09 23:52:12 +0530

    assignment - 01 done (added task - 02)

---
+++ LAB-01/src/src/Task - 02/task -02.cpp
@@ -0,0 +1,24 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    // Set PB0 as output pin
+    DDRB |= (1 << DDB0);
+
+    while (1)
+    {   
+        //The LED connected to PB0 is toggled ON and OFF using PORTB register. The delay function _delay_ms(1000) creates a visible 1-second blink interval.
+        // LED ON (PB0 HIGH)
+        PORTB |= (1 << PB0);
+        _delay_ms(1000);
+
+        // LED OFF (PB0 LOW)
+        PORTB &= ~(1 << PB0);
+        _delay_ms(1000);
+    }
+
+    return 0;
+}
